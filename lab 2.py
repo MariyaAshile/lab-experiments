13bd65318f8c7b96b2244471057e23062d3913b9
MariyaAshile <138784422+MariyaAshile@users.noreply.github.com>
2023-07-06 19:51:26 +0530

    Add files via upload

---
+++ lab 2.py
@@ -0,0 +1,14 @@
+def check(st1,st2):
+    i=0
+    j=0
+    while i<len(st1) and j<len(st2) :
+        if (st1[i]==st2[j]):
+            j+=1
+        i+=1
+    return j==len(st2)
+st1=input("enter string1: ")
+st2=input("enter string2: ")
+if check(st1,st2):
+    print("YES")
+else:
+    print("NO")
