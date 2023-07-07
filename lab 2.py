f2ff4e54a64b165534efe553307135e438f326ce
MariyaAshile <138784422+MariyaAshile@users.noreply.github.com>
2023-07-07 14:43:34 +0530

    h

--- lab 2.py
+++ lab 2.py
@@ -6,7 +6,7 @@ def check(st1,st2):
             j+=1
         i+=1
     return j==len(st2)
-st1=input("enter string1: ")
+st1=input("enter string1 b: ")
 st2=input("enter string2: ")
 if check(st1,st2):
     print("YES")
